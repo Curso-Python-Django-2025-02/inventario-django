b57be3cfe2543d40c3beecd646ed8744e9034633
Jairo Chapela-Martínez <jairochapela@gmail.com>
2025-02-20 19:00:48 +0100

    Jerarquía de plantillas, rutas modulares.

--- activos/urls.py
+++ activos/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from activos.views import ActivoDetailView, ActivoListView
 
+app_name = 'activos'
 urlpatterns = [
-    path('', ActivoListView.as_view(), name='activos:index'),
-    path('<int:pk>/', ActivoDetailView.as_view(), name='activos:detail'),
+    path('', ActivoListView.as_view(), name='index'),
+    path('<int:pk>/', ActivoDetailView.as_view(), name='detail'),
 ]
--- inventario/urls.py
+++ inventario/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('activos/', include('activos.urls')),
+    path('activos/', include('activos.urls', namespace='activos')),
+    path('', lambda _: redirect('activos:index'), name='home'),
 ]
